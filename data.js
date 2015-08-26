92d0da8f3033da06fedf1f39b5798a052e22921e
umbobabo <umbertobabini@economist.com>
2015-08-26 12:16:05 +0100

    Added mockup data

---
+++ data.js
@@ -0,0 +1,60 @@
+export default {
+  company: [
+      {
+        link_title: "Advertise",
+        link_path: "http:\/\/www.economistgroupmedia.com"
+      },
+      {
+        link_title: "Reprints",
+        link_path: "http:\/\/www.economist.com\/rights\/"
+      },
+      {
+        link_title: "Careers",
+        link_path: "http:\/\/jobs.economist.com"
+      },
+      {
+        link_title: "Editorial Staff",
+        link_path: "mediadirectory"
+      },
+      {
+        link_title: "Site Map",
+        link_path: "content\/site-index"
+      },
+      {
+        link_title: "The Economist Group",
+        link_path: "http:\/\/www.economistgroup.com"
+      }
+    ],
+    contact: [
+      {
+        link_title: "Subscribe",
+        link_path: "products\/subscribe"
+      },
+      {
+        link_title: "Contact us",
+        link_path: "node\/21017280"
+      },
+      {
+        link_title: "Help",
+        link_path: "node\/21013080"
+      }
+    ],
+    legal: [
+      {
+        link_title: "Accessibility",
+        link_path: "node\/21013093"
+      },
+      {
+        link_title: "Cookies",
+        link_path: "node\/21554326"
+      },
+      {
+        link_title: "Privacy",
+        link_path: "http:\/\/www.economistgroup.com\/results_and_governance\/governance\/privacy"
+      },
+      {
+        link_title: "Terms of use",
+        link_path: "node\/21010345"
+      }
+    ]
+  };
